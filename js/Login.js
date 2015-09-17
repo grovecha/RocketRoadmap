9e18370cab57b4f70d56d3943df91c1f3a4393e8
Chase <grovecha@msu.edu>
2015-09-16 23:19:45 -0400

    testing out login templates

---
+++ js/Login.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Chase on 9/16/2015.
+ */
+
+
+function validateCredentials()
+{
+    window.loccation.href = "Home.html";
+    if(document.getElementById("email_ID")!="" && document.getElementById("password_ID")!="") {
+
+    }
+}
