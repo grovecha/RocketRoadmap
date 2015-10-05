3ce61536497f0ef6cfdee654fe5ce3149d18aa2e
tothbenc <btoth1@gmail.com>
2015-10-04 22:43:41 -0400

    -added data input -commented out table within sidebar -add button on project add -added showModal function

--- Test/Test/Roadmap1.aspx.designer.cs
+++ Test/Test/Roadmap1.aspx.designer.cs
@@ -38,5 +38,14 @@ namespace Test {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TableRow Strat1;
+        
+        /// <summary>
+        /// PlaceHolder1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl PlaceHolder1;
     }
 }
