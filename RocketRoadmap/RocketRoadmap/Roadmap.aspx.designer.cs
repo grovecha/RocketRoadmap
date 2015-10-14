9550bac2dd05d3b9c7871fec4e19de802e8634b9
tothbenc <btoth1@gmail.com>
2015-10-13 20:02:05 -0400

    roadmap.aspx

--- RocketRoadmap/RocketRoadmap/Roadmap.aspx.designer.cs
+++ RocketRoadmap/RocketRoadmap/Roadmap.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace RocketRoadmap {
     public partial class Roadmap {
         
         /// <summary>
-        /// RoadmapTitle control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlTitle RoadmapTitle;
-        
-        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
