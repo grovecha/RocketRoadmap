be9d9fc18c03b7d531b552e0519dd71543a69044
bpchiv <bpchiv@gmail.com>
2015-11-04 23:00:59 -0500

    Continued Text Clean Up

--- RocketRoadmap/RocketRoadmap/DB/Link.cs
+++ RocketRoadmap/RocketRoadmap/DB/Link.cs
@@ -6,16 +6,22 @@ using System.Data.SqlClient;
 
 namespace RocketRoadmap.DB
 {
+    //Class for links that projects own
     public class Link
     {
+        //Project that owns it
         private string mProjectName;
+        //Description of link
         private string mDescription;
+        //Actual link or address
         private string mLink;
+        //Roadmap this belongs to
         private string mRoadmapName;
 
         private RocketRoadmap.DB.Database mDatabase = new RocketRoadmap.DB.Database();
         private SqlDataReader mReader;
 
+        //Constructor
         public Link(string descrip, string projname, string link, string RoadmapName)
         {
             mProjectName = projname;
@@ -27,9 +33,18 @@ namespace RocketRoadmap.DB
         #region Getter's and Setter's
         public bool SetDescription(string description)
         {
+            //Connect to DB and change in DB
             mDatabase.connect();
-            bool flag = mDatabase.executewrite("UPDATE [dbo].[Link] SET Description='" + description + "' WHERE Description='" + mDescription + "' AND RoadmapName='" + mRoadmapName + "'");
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "UPDATE [dbo].[Link] SET Description=@descrip WHERE Description=@mDescrip AND RoadmapName=@Rname";
+            cmd.Parameters.AddWithValue("@descrip", description);
+            cmd.Parameters.AddWithValue("@mDescrip", mDescription);
+            cmd.Parameters.AddWithValue("@Rname", mRoadmapName);
+            bool flag = mDatabase.executewriteparam(cmd);
             mDatabase.close();
+
+            //Change in object
+            mDescription = description;
             return flag;
         }
         public string GetDescription()
@@ -38,9 +53,18 @@ namespace RocketRoadmap.DB
         }
         public bool SetProjectName(string projectname)
         {
+            //Change in DB
             mDatabase.connect();
-            bool flag = mDatabase.executewrite("UPDATE [dbo].[Link] SET ProjectName='" + projectname + "' WHERE Description='" + mDescription + "' AND RoadmapName='" + mRoadmapName + "'");
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "UPDATE [dbo].[Link] SET ProjectName=@Proj WHERE Description=@mDescrip AND RoadmapName=@Rname";
+            cmd.Parameters.AddWithValue("@Proj", projectname);
+            cmd.Parameters.AddWithValue("@mDescrip", mDescription);
+            cmd.Parameters.AddWithValue("@Rname", mRoadmapName);
+            bool flag = mDatabase.executewriteparam(cmd);
             mDatabase.close();
+
+            //Change in object
+            mProjectName = projectname;
             return flag;
         }
         public string GetProjectName()
@@ -49,9 +73,18 @@ namespace RocketRoadmap.DB
         }
         public bool SetLink(string link)
         {
+            //Change in DB
             mDatabase.connect();
-            bool flag = mDatabase.executewrite("UPDATE [dbo].[Link] SET Address='" + link + "' WHERE Description='" + mDescription + "' AND RoadmapName='" + mRoadmapName + "'");
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "UPDATE [dbo].[Link] SET Address=@link WHERE Description=@mDescrip AND RoadmapName=@Rname";
+            cmd.Parameters.AddWithValue("@link", link);
+            cmd.Parameters.AddWithValue("@mDescrip", mDescription);
+            cmd.Parameters.AddWithValue("@Rname", mRoadmapName);
+            bool flag = mDatabase.executewriteparam(cmd);
             mDatabase.close();
+
+            //Change in object
+            mLink = link;
             return flag;
         }
         public string GetLink()
