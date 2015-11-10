ee43eeac3ae062360d26ab11372b6a0467c460d4
bpchiv <bpchiv@gmail.com>
2015-11-10 17:55:31 -0500

    changed clicking bs

--- RocketRoadmap/RocketRoadmap/js/Roadmap.js
+++ RocketRoadmap/RocketRoadmap/js/Roadmap.js
@@ -431,8 +431,8 @@ function addProj(e, obj, i) {
                 element1.innerHTML =  NewValue;
             }
             //element1.style.verticalAlign = "top";
-            element1.setAttribute("onclick", "showMode(this.id)");
-            element1.setAttribute("onmouseenter", "Highlight(this.id)");
+            element1.setAttribute("ondblclick", "showMode(this.id)");
+            element1.setAttribute("onclick", "Highlight(this.id)");
             element1.setAttribute("onresize", "disableModal()");
             element1.setAttribute("ondrag", "disableModal()");
             element1.setAttribute("onmouseleave", "UnHighlight(this.id)");
