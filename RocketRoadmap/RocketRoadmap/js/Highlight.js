b57ecf77e926056bb12eea49edf805049c4c5b65
klopfere <klopfere@msu.edu>
2015-11-29 15:44:50 -0500

    highlight

--- RocketRoadmap/RocketRoadmap/js/Highlight.js
+++ RocketRoadmap/RocketRoadmap/js/Highlight.js
@@ -8,7 +8,6 @@ var depon_arr = [];
 var depof_arr = [];
 var dtemp1 = [];
 var dtemp2 = [];
-var color = ["#DC381F", "#33cccc", "#6CBB3C", "#A23BEC", "#F87217", "#157DEC"];
 
 function Highlight(id) {
     var cheche = "#" + id;
@@ -72,9 +71,10 @@ function Uncolor(id) {
 
         var temp = id[id_x].split("StratBox");
         temp = temp[1].split("BusBox")[0]
-        var c = color[temp%6];
 
-        $(id[id_x]).css('background-color', c);
+        var return_c = document.getElementById("ColorPicker" + temp).value;
+
+        $(id[id_x]).css('background-color', return_c);
     }
 }
 
@@ -83,9 +83,9 @@ function UnHighlight(id) {
     var cheche = "#" + id;
     var temp = id.split("StratBox");
     temp = temp[1].split("BusBox")[0]
-    var c = color[temp%6];
+    var return_c = document.getElementById("ColorPicker" + temp).value;
 
-    $(cheche).css({ "background-color": c });
+    $(cheche).css({ "background-color": return_c });
 
     Uncolor(depon_arr);
     Uncolor(depof_arr);
